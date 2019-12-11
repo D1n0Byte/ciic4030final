a1b21af52cb54f63229ef67cf3c74aa3c76f794f
Julio Aguilar <julio.aguilar@upr.edu>
2019-12-11 18:44:02 -0400

    added rules for tokens

--- lexer.py
+++ lexer.py
@@ -1,4 +1,4 @@
-from sly import lexer
+from sly import Lexer
 
 class SucketLexer(Lexer):
     tokens= {
@@ -13,5 +13,19 @@ class SucketLexer(Lexer):
     LISTEN, 
     ACCEPT
     }
-    # ignore=
+
+    #strings containing ignored char between tokens
+    ignore= ' \t'
     # literals= 
+
+    #Rules for Tokens
+    CREATE   = 'create'
+    SERVER   = 'server'
+    CONNECT  = 'connect'
+    RECEIVE  = 'receive'
+    SEND     = 'send'
+    CLIENT   = 'client'
+    CLOSE    = 'close'
+    BIND     = 'bind'
+    LISTEN   = 'listen'
+    ACCEPT   = 'accept'
